e54e687b51ae092fdba4713baea1829fb291acdd
Nicole <nicole_a_alejandro@live.com>
2019-02-13 17:47:55 -0800

    New Migration. Added DbSet<Mood>.

--- MoodMe/Data/ApplicationDbContext.cs
+++ MoodMe/Data/ApplicationDbContext.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using MoodMe.Models;
 
 namespace MoodMe.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-
+        public DbSet<Mood> Moods { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ MoodMe/Data/Migrations/20190214014603_Mood.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoodMe.Data.Migrations
+{
+    public partial class Mood : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Moods",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moods", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Moods");
+        }
+    }
+}
