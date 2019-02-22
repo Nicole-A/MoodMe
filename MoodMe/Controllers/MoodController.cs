7be1e2d4b1ce5aa8f7920037a49edf157da47d15
Nicole <nicole_a_alejandro@live.com>
2019-02-21 19:21:10 -0800

    Mood Controller and Mood/Index - Moods displaying Need to figure out dropdown

--- MoodMe/Controllers/MoodController.cs
+++ MoodMe/Controllers/MoodController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MoodMe.Data;
+using MoodMe.ViewModels;
+using MoodMe.Models;
 
 namespace MoodMe.Controllers
 {
     public class MoodController : Controller
     {
+        private readonly ApplicationDbContext context;
+
+        public MoodController(ApplicationDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(context.Moods.ToList());
         }
     }
 }
